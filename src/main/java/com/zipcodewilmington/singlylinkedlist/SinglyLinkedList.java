bcb81bb343e84e570b8078fe01e04610606bda5c
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-03-09 09:24:27 -0400

    added method stub

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,20 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    public void addMethod(){}
+
+    public void removeMethod(){}
+
+    public void containsMethod(){}
+
+    public void getMethod(){}
+
+    public void sizeMethod(){}
+
+    public void findMethod(){}
+
+    public void copyMethod(){}
+
+    public void sortMethod(){}
 }
